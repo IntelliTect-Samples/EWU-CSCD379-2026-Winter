4e5901daaa856ebce9115ba56763a8bb627cedc9
Chloe Stanton <cstanton5@ewu.edu>
2026-02-04 18:27:35 -0800

    replaced build with generate

--- game-app/nuxt.config.ts
+++ game-app/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  nitro: {
+    preset: 'azure-swa'
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: [
