e2562a3e275ef115f768153518a5543f7746c06d
Chloe Stanton <cstanton5@ewu.edu>
2026-02-21 12:47:10 -0800

    config update

--- florist-app/nuxt.config.ts
+++ florist-app/nuxt.config.ts
@@ -34,6 +34,8 @@ export default defineNuxtConfig({
     }
   },
   css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css',
     '@/assets/css/index.css',
     '@/assets/css/login.css',
     '@/assets/css/NavigationBar.css',
