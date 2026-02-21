48bd84ee162274c379910a092f3f37c8a9828dad
Chloe Stanton <cstanton5@ewu.edu>
2026-02-21 11:11:39 -0800

    Frontend deploy test

--- florist-app/nuxt.config.ts
+++ florist-app/nuxt.config.ts
@@ -35,7 +35,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://bs-botanicals-api-dtgjbhfwacheb7bb.eastus2-01.azurewebsites.net/api'
+      apiBase: 'https://bs-botanicals-api-dtgjbhfwacheb7bb.eastus2-01.azurewebsites.net/api'
     }
   }
 })
